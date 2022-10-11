88076ad51acf1ab2e4fc97cd3073210716cd8092
aroy15 <anjonroy57@gmail.com>
2022-10-11 16:00:46 +0600

    nav bar design complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <nav>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/statistics'>Statistics</NavLink>
-                <NavLink to='/blog'>Blog</NavLink>
+            <nav className="navbar navbar-expand-lg bg-light shadow">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Front-end's Quiz</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink to='/' className={({isActive})=> isActive?'nav-active nav-link':'nav-link'}>Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to='/statistics' className={({isActive})=> isActive?'nav-active nav-link':'nav-link'}>Statistics</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to='/blog' className={({isActive})=> isActive?'nav-active nav-link':'nav-link'}>Blog</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
     );
