28898e50933673a55f4ce25cdf71d06b0bccb6c3
aroy15 <anjonroy57@gmail.com>
2022-10-12 19:21:48 +0600

    line chart responsive issue fix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 
 
 const Statistics = () => {
     const loadMainData = useLoaderData();
-    console.log(loadMainData);
     const data = [];
-    // {name: 'Page A', uv: 100, pv: 2400, amt: 2400},
-    //     {name: 'Page B', uv: 300, pv: 2400, amt: 2400},
-    //     {name: 'Page C', uv: 200, pv: 2400, amt: 2400},
-    //     {name: 'Page D', uv: 140, pv: 2400, amt: 2400},
+
     loadMainData.data.map((singleData) => {
         const {name,total} = singleData;
         const dataObj = {name,uv:total,pv:2400,amt:2400}
@@ -21,12 +17,14 @@ const Statistics = () => {
             <div className="container">
                 <h1 className='text-center mb-4'>Statistics</h1>
                 <div className='w-100 d-flex justify-content-center'>
-                    <LineChart width={800} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                    </LineChart>
+                    <ResponsiveContainer width='100%' aspect={3}>
+                        <LineChart width={800} height={300} data={data} >
+                            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                        </LineChart>
+                    </ResponsiveContainer>
                 </div>
             </div>
         </section>
