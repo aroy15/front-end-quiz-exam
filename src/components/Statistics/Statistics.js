4af68f2c8551ba32772bf69ab41b9d573506045f
aroy15 <anjonroy57@gmail.com>
2022-10-12 18:49:33 +0600

    rechart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
 
 const Statistics = () => {
+    const loadMainData = useLoaderData();
+    console.log(loadMainData);
+    const data = [];
+    // {name: 'Page A', uv: 100, pv: 2400, amt: 2400},
+    //     {name: 'Page B', uv: 300, pv: 2400, amt: 2400},
+    //     {name: 'Page C', uv: 200, pv: 2400, amt: 2400},
+    //     {name: 'Page D', uv: 140, pv: 2400, amt: 2400},
+    loadMainData.data.map((singleData) => {
+        const {name,total} = singleData;
+        const dataObj = {name,uv:total,pv:2400,amt:2400}
+        data.push(dataObj)
+    })
     return (
-        <div>
-            <h1>This is statistics page</h1>
-        </div>
+        <section className='py-5 mt-4'>
+            <div className="container">
+                <h1 className='text-center mb-4'>Statistics</h1>
+                <div className='w-100 d-flex justify-content-center'>
+                    <LineChart width={800} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                    </LineChart>
+                </div>
+            </div>
+        </section>
     );
 };
 
