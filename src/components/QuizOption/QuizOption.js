017ca3e580b868b63c72dd82c87e455ac0609eaa
aroy15 <anjonroy57@gmail.com>
2022-10-12 16:51:48 +0600

    opction box design fix

--- src/components/QuizOption/QuizOption.js
+++ src/components/QuizOption/QuizOption.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './QuizOption.css'
 const QuizOption = ({option,optionIndex, questionIndex, questionResultPopup, correctAnswer}) => {
     return (
-        <div className="col-md-6">
-            <label onClick={()=>questionResultPopup(correctAnswer, option, optionIndex)} className='option-item d-flex align-items-center gap-2 py-4 px-3 border border-success rounded pointer'>
+        <div className="col-md-6 d-flex">
+            <label onClick={()=>questionResultPopup(correctAnswer, option, optionIndex)} className='option-item d-flex align-items-center gap-2 py-4 px-3 border border-success rounded pointer w-100'>
                 <input type="radio" name={`quiz-${questionIndex}`} />
                 <span className='text-success fw-bold'>{option}</span>
             </label>
