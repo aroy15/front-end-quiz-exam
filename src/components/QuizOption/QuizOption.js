95b31e90c9986f5833f1346908d168f223167c0f
aroy15 <anjonroy57@gmail.com>
2022-10-12 15:33:26 +0600

    react toast added

--- src/components/QuizOption/QuizOption.js
+++ src/components/QuizOption/QuizOption.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './QuizOption.css'
-const QuizOption = ({option,questionIndex}) => {
+const QuizOption = ({option,optionIndex, questionIndex, questionResultPopup, singleToast}) => {
     return (
         <div className="col-md-6">
-            <label className='option-item d-flex align-items-center gap-2 py-4 px-3 border border-success rounded pointer'>
+            <label onClick={()=>questionResultPopup(option,optionIndex)} className='option-item d-flex align-items-center gap-2 py-4 px-3 border border-success rounded pointer'>
                 <input type="radio" name={`quiz-${questionIndex}`} />
                 <span className='text-success fw-bold'>{option}</span>
             </label>
+            {/* {singleToast} */}
         </div>
-        
     );
 };
 
 export default QuizOption;
+// onClick={()=>questionResultPopup()}
--- src/components/QuizQuestionCard/QuizQuestionCard.js
+++ src/components/QuizQuestionCard/QuizQuestionCard.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import QuizOption from '../QuizOption/QuizOption';
-import './QuizQuestionCard.css'
+import './QuizQuestionCard.css';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizQuestionCard = (props) => {
-    const {options, question} = props.question;
+    const {options, question, correctAnswer} = props.question;
+    console.log(question)
+    // React Toast
+    const questionResultPopup = (option, questionIndex) =>{
+        if(correctAnswer===option){
+            toast.success(`Correct Answer 😃`,{
+                position: "top-center",
+                toastId: `option-${questionIndex}`
+            })
+        }else{
+            toast.warning(`Incorrect Answer 😥`,{
+                position: "top-center",
+                toastId: `option-${questionIndex}`
+            })
+        }
+    }
     return (
         <div className='quiz-question-card shadow rounded mt-5 p-4'>
             <h5 className='mb-4 text-center'>{`Quiz ${props.questionIndex+1}: ${question}`}</h5>
-            <div className="row g-2 justify-content-center">                
+            <div  className="row g-2 justify-content-center">                
                 {
-                    options.map((option,index)=><QuizOption option={option} key={index} questionIndex={props.questionIndex}></QuizOption>)
+                    options.map((option,index)=><QuizOption 
+                        option={option} 
+                        key={index} 
+                        questionIndex={props.questionIndex} 
+                        questionResultPopup={questionResultPopup}
+                        optionIndex={index}
+                        // singleToast={<ToastContainer></ToastContainer>}
+                    ></QuizOption>)
                 }
             </div>
+                <ToastContainer></ToastContainer>
         </div>
     );
 };
